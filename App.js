64b6002ecb98b25fddc1e5f30f37555c3f645a4d
Alexandre Tournel <alexandre.tournel@lacatholille.fr>
2024-03-26 15:54:23 +0100

    Correction Bug Improt

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import {NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './Screens/HomeScreen';
 import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 const Stack = createStackNavigator();
 
