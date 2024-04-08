b1d0cec6d2b8224f047dafe67efec63c6a06444e
jeanBrbt <101502668+jeanBrbt@users.noreply.github.com>
2024-04-08 11:35:08 +0200

    navigator fix
     navigator fix
    
     navigator fix

--- App.js
+++ App.js
@@ -1,45 +1,37 @@
 import {NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from "./Screens/HomeScreen";
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './Screens/HomeScreen';
 import LoginPage from "./Screens/LoginPage";
 import LandingPage from "./Screens/LandingPage";
 import RegisterPage from "./Screens/RegisterPage";
 import GameSelectionPage from "./Screens/GameSelectionPage";
 import GamePage from "./Screens/GamePage";
 import GameSettingsPage from "./Screens/GameSettingsPage";
-const Stack = createStackNavigator();
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+function MyStack() {
+  return (
+    <NavigationContainer>
+    <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Landing" component={LandingPage}/>
+        <Stack.Screen name="Login" component={LoginPage}/>
+        <Stack.Screen name="Register" component={RegisterPage}/>
+        <Stack.Screen name="GameSelection" component={GameSelectionPage}/>
+        <Stack.Screen name="Game" component={GamePage}/>
+        <Stack.Screen name="GameSettings" component={GameSettingsPage}/>
+        </Stack.Navigator>
+    </NavigationContainer>
+
+  );
+}
+const  App= ()=> {
     return (
-        <NavigationContainer>
-            <Stack.Navigator>
-
-                <Stack.Screen name="Landing" component={LandingPage}/>
-                <Stack.Screen name="Login" component={LoginPage}/>
-                <Stack.Screen name="Register" component={RegisterPage}/>
-                <Stack.Screen name="Home" component={HomeScreen}/>
-                <Stack.Screen name="GameSelection" component={GameSelectionPage}/>
-                <Stack.Screen name="Game" component={GamePage}/>
-                <Stack.Screen name="GameSettings" component={GameSettingsPage}/>
-
-
-            </Stack.Navigator>
-        </NavigationContainer>
+        <MyStack/>
     );
 }
 
-
-
-
-
-
-const styles = StyleSheet.create({
-    container: {
-        width: 400,
-        height: 100,
-        marginBottom: 250,
-
-    },
-});
+export default App;
